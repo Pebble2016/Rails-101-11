9c76c9e9d02e983066aa159d1dae9a959ee5a53c
pebble <ypj2005@126.com>
2017-11-09 17:05:08 +0800

    show group's creator info

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 20171109081359) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
